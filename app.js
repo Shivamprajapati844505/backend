065c2fc40c2433a9c42d71d9ed951b424ac44963
tewarishash12 <tewarishashwat2002@gmail.com>
2024-11-02 18:08:54 +0530

    updated frontend link

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5173', // Replace with your client origin
+    origin: 'https://project-management-list-frontend.onrender.com/', // Replace with your client origin
     credentials: true, // Allow cookies to be sent
 };
 
